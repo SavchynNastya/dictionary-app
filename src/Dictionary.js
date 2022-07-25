7f6c33430d30fcb03abdb4c826fd8ff1ff2fb384
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-07-25 16:20:02 +0300

    axios, api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./search-engine-styles.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function searchWord(event) {
     event.preventDefault();
-    return alert(`${word} word`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    return axios.get(apiUrl).then(handleResponse);
   }
 
   function changeWord(event) {
